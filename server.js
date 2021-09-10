c71467af7854fe461aa3e689103112012b922650
Mia <micaelasgilbert@gmail.com>
2021-09-10 11:38:58 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+module.exports = app;
+
+/* Do not change the following line! It is required for testing and allowing
+*  the frontend application to interact as planned with the api server
+*/
+const PORT = process.env.PORT || 4001;
+
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res, next) => { 
+  res.sendFile('index.html', { root: __dirname });
+});
+
+// Add middleware for handling CORS requests from index.html
+app.use(cors());
+
+// Add middware for parsing request bodies here:
+app.use(bodyParser.json());
+
+// Logging
+app.use(morgan('dev'));
+
+// Mount your existing apiRouter below at the '/api' path.
+const apiRouter = require('./server/api');
+app.use('/api', apiRouter);
+
+// Add your code to start the server listening at PORT below:
+app.listen(PORT, () => {
+    console.log(`Server listening on Port ${PORT}`)
+});
